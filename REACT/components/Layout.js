b81f0080585c2fbaf5b8c5370c9d3b6d6fc14031
oyavuzjr <oyavuz@g.ucla.edu>
2021-03-12 22:42:56 -0800

    refactored toaster

--- REACT/components/Layout.js
+++ REACT/components/Layout.js
@@ -1,12 +1,11 @@
 import Nav from "./Nav";
-import Link from "next/link";
-import { useStoreState, useStoreActions } from "easy-peasy";
+import { useStoreState } from "easy-peasy";
 import { useEffect } from "react";
-import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
 import { useStoreRehydrated } from "easy-peasy";
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga';
+import Toaster from './Toaster';
 const Layout = ({ children }) => {
   const notifications = useStoreState((state) => state.toaster.notifications);
   const router = useRouter();
@@ -36,52 +35,7 @@ const Layout = ({ children }) => {
           <div className="container">{children}</div>
           {notifications.length > 0 ? (
             <div style={{ position: "fixed", bottom: "20px", right: "20px" }}>
-              <motion.div
-                variants={{
-                  hidden: { scale: 0.6, opacity: 0 },
-                  visible: { scale: 1, opacity: 1, transition: { delay: 0.2 } },
-                }}
-                initial="hidden"
-                animate={notifications.length > 0 ? "visible" : "hidden"}
-              >
-                <div
-                  className="toast show"
-                  role="alert"
-                  aria-live="assertive"
-                  aria-atomic="true"
-                >
-                  <div className="toast-header">
-                    <strong className="mr-auto">Success!</strong>
-                    <small>now</small>
-                    <button
-                      type="button"
-                      className="ml-2 mb-1 close"
-                      data-dismiss="toast"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <Link href="/cart">
-                    <a>
-                      <div className="toast-body gray-hover">
-                        <div className="row">
-                          <div className="col col-3 text-center">
-                            <img
-                              width={50}
-                              height={75}
-                              src={notifications[notifications.length - 1].img}
-                            />
-                          </div>
-                          <div className="col justify-content-center align-self-center">
-                            {notifications[notifications.length - 1].message}
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
-                </div>
-              </motion.div>
+                <Toaster/>
             </div>
           ) : (
             ""
